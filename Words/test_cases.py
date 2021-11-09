f1f912d5383ac0f27aa24337c5b6159073bc128e
DevTeymur <teymurr.601@gmail.com>
2021-11-09 16:50:23 +0400

    Add lemmatizer

--- Words/test_cases.py
+++ Words/test_cases.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 test1="Biz Suriyaya, Əfqanıstana baxıb şükür edə bilmərik. Biz Azərbaycan sərhədlərindən kənarda Azərbaycan qurmuşuq. 100 il əvvəlki Azərbaycanı qoruya bilsək bugün nümunə göstərilən biz olardıq. Bizim Xoyskimiz, Topçubaşovumuz, Cavidimiz, Zərdabimiz və s. olub. Biz istifadə edə bilmədiyimiz potensialımıza heyifsilənməliyik. Finlandiya ola bilməyimizin qarşısında duran səriştəsizliyimizlə mübarizə aparmalıyıq."
 
 test2="Şəkil Napolidə yerləşən bir metro stansiyasından imiş. Biz hələ də kondisionerli qatar gözləyirik."
@@ -18,63 +16,9 @@ test8="Əmək müqaviləsi Azerbaycanin və ABŞ bildirişi” e-sistemində 12 
 
 string_list = [test1, test2, test3, test4, test5, test6, test7, test8]
 
-def AppendToDataframe(df, stirnglist=string_list):
-    for text in string_list:
+def AppendToDataframe(df, stringlist=string_list):
+    for text in stringlist:
         d = {'Text': text}
         df=df.append(d, ignore_index=True)
         d={}
     return df
-
-'''
-f = open("words.txt", "r",  encoding="utf8")
-words = f.readlines()
-
-str1 = "".join(words)
-words = str1.split("\n")
-
-# moterze reqemler tire olkeler adlar seherler
-
-
-def empty(i):
-    if i != "" and i != "," and i != ":" and i != "\"":
-        return True
-    else:
-        return False
- 
-
-def check(test):
-    l = []
-    vowels = ["a", "ı", "o", "u", "e", "ə", "i"]
-    test = test.split()
-    print(test)
-    for i in test:
-        if i.isupper() or (test.index(i) != 0 and i[0].isupper()):
-            # xüsusi isimlər və abbr. üçün
-            l.append(i)
-            print(i)
-        else:
-            for j in range(len(i), 0, -1):
-                if i[:j].casefold() in words:
-                    print(i[:j])
-                    # adi şəkilçilər üçün
-                    #l.append( "".join(   i[0:].split(i[:j]) )  )
-                    l.append(i[:j])
-                    break
-                elif i[j-1] in vowels and (i[j-2] == "y" or i[j-2] == "ğ"):
-                    # bitişdirici samitlər üçün
-                    if (i[:j-2]+"k").casefold() in words:
-                        print(i[:j-2]+"k")
-                        l.append(i[:j-2]+"k")
-                        break
-                    elif (i[:j-2]+"q").casefold() in words:
-                        print(i[:j-2]+"q")
-                        l.append(i[:j-2]+"q")
-                        break
-
-    # print(l)
-    # return set(filter(empty,l))
-    return l
-
-
-check(tc.test5)
-'''
