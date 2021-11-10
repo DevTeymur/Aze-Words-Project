56d7d0d52b871cb331d4b432251202c96f12b708
DevTeymur <teymurr.601@gmail.com>
2021-11-10 22:39:23 +0400

    Error in lemmatize part

--- Words/test.py
+++ Words/test.py
@@ -1,8 +1,11 @@
+from functools import partial
+import re
 import pandas as pd
 import numpy as np
 from file_reader import suffixes
+from filter import Search
 
-s=suffixes()
+s=sorted(suffixes(), key=len)[::-1]
 
 df=pd.DataFrame({'A': [1,2,3,4,5,6], 'B': [1,2,3,4,5,6], 'C': [1,2,3,4,5,6]})
 
@@ -10,9 +13,28 @@ df=pd.DataFrame({'A': [1,2,3,4,5,6], 'B': [1,2,3,4,5,6], 'C': [1,2,3,4,5,6]})
 df.loc[len(df)]=np.nan
 df.loc[len(df)-1, 'B']='7'
 
-word='kitablara'
-for i in s:
-    if word.endswith(i):
-        word=word.replace(i, "")
+word='torpağı' 
+word1='kitablara'
+
+
+a=word
+run=True
+if Search(a):
+    print('Yes')
+else:
+    while run:
+        for i in range(len(s)):
+            if a.endswith(s[i]):
+                print(s[i])
+                a=a[::-1].replace(s[i][::-1], "", 1)[::-1]
+                print(a)
+                break
+            
+        if Search(a):
+            run=False
+            break
+        else:
+            break
 
 print(word)
+print(a)
