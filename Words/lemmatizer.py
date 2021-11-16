5e7fdd7c38b1dceb5b07954ab2bb4dbe7eacae3f
DevTeymur <teymurr.601@gmail.com>
2021-11-16 15:38:02 +0400

    #2

--- Words/lemmatizer.py
+++ Words/lemmatizer.py
@@ -5,3 +5,5 @@ import os
 df = pd.read_csv(f'{os.path.abspath(os.path.join(os.path.dirname(__file__),".."))}/Words/result.csv')
 df= Lemmatizer(df)
 df.to_csv(f'{os.path.abspath(os.path.join(os.path.dirname(__file__),".."))}/Words/final.csv')
+
+
